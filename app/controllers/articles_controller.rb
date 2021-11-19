d1d779a36f6f2ee7fdecff2400b6d10734649d8f
Laszlo Borsos <laszlo.borsos1@hu.ibm.com>
2021-11-19 13:53:08 +0100

    modified:   app/controllers/articles_controller.rb 	new file:   app/controllers/pages_controller.rb 	modified:   app/models/article.rb 	new file:   app/views/articles/edit.html.erb 	modified:   app/views/articles/index.html.erb 	new file:   app/views/articles/new.html.erb 	modified:   app/views/articles/show.html.erb 	modified:   app/views/layouts/application.html.erb 	new file:   app/views/pages/about.html.erb 	new file:   app/views/pages/home.html.erb 	modified:   config/routes.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,49 @@ class ArticlesController < ApplicationController
     def index
         @articles=Article.all 
     end
+    def create
+        #render plain: params[:article]  #this is just for  debug , shows json structure 
+        #@article = Article.new(params[:article])  #it is not allowed method because of security 
+        # we have to whitelist the parameters that comes in 
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        #render plain: params[:article].inspect  # this is just for debug 
+
+      
+        if @article.save   # save the record 
+        #redirect_to article_path(@article)   # redirect to article show 
+            flash[:notice] = "Article was ceated successfully!"
+            # this uses a flash helper . Can be notice or alert .
+            redirect_to @article  # same as above 
+        else
+            render 'new'
+        
+        end
+
+    end
+    def new
+        @article = Article.new  #this is because the article variable will not exit 
+        # when we first open the view form  , just after we sent to create 
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice]="Article was updated successfully!"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :articles ,only: [:show, :index]
+  #resources :articles ,only: [:show, :index, :create, :new, :edit, :update, :destroy]
+
+  root 'pages#home'
+  get 'about',to: 'pages#about'
+
+
+  resources :articles 
+  
 end
