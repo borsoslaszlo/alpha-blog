7530e4ffb1a342b7d65efa053a6102bd7f7bdd9d
Laszlo Borsos <laszlo.borsos1@hu.ibm.com>
2021-12-01 07:49:21 +0100

    Cleanup layout 	modified:   app/controllers/users_controller.rb 	modified:   app/views/articles/_article.html.erb 	modified:   app/views/articles/show.html.erb 	modified:   app/views/users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
         @user= User.new(user_params)
         if @user.save
             flash[:notice]="Welcome to the Alpha Blog #{@user.username}, you have successfully signed in!"
-            redirect_to articles_path
+            redirect_to user_path
         else
             render 'new'
         end
