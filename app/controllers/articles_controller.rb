c3bd939add0809060d678f15a353189ef94184f4
Laszlo Borsos <laszlo.borsos1@hu.ibm.com>
2021-12-01 14:50:45 +0100

    REstrict action for users conmtroller  -   not to bypass the modifications by URL modification in user controller 	modified:   app/controllers/articles_controller.rb 	modified:   app/controllers/users_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,7 +65,7 @@ class ArticlesController < ApplicationController
     end
     def require_same_user
         if current_user!= @article.user
-        flash[:alert] = "You can only edit or delete you own article"
+        flash[:alert] = "You can only edit or delete your own article!"
         redirect_to @article
         end
 
