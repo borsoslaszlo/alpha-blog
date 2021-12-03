df3644cf0b9fd8bb2d84f9bbf62c2fa5142eb6af
Laszlo Borsos <laszlo.borsos1@hu.ibm.com>
2021-12-03 09:53:08 +0100

    Admin user requirements and test  (only admin can create categories)
    	modified:   app/controllers/categories_controller.rb
    	modified:   test/controllers/categories_controller_test.rb
    	modified:   test/integration/create_category_test.rb
    	modified:   test/test_helper.rb

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,12 +1,23 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+      @admin_user = User.create(username: "johndoe",email: "johndoe@johndoe.com",
+      password: "password",admin: true)
+    
+    sign_in_as(@admin_user)
+    
+  end
+
+
+
   test "getting new form and create category" do
     get "/categories/new"
     assert_response :success
     assert_difference 'Category.count',1 do
-    post categories_path , params: {category:{name: "Sports"} }
-    assert_response :redirect
+      post categories_path , params: {category:{name: "Sports"} }
+      assert_response :redirect
     end
     follow_redirect!
     assert_response :success
@@ -17,8 +28,7 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     get "/categories/new"
     assert_response :success
     assert_no_difference 'Category.count' do
-    post categories_path , params: {category:{name: ""} } #empty submission
-  
+      post categories_path , params: {category:{name: ""} } #empty submission
     end
     assert_match "errors" , response.body 
     assert_select 'div.alert'
